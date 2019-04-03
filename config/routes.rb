bbf69193bc9791c57432949bd13df5a2bef1dde8
josereyessaldias <jpreyess@uc.cl>
2019-04-02 23:15:51 -0300

    ahora a empezar user_activity

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   devise_for :users, controllers: {
         registrations: 'user/registrations'
       }
 
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_01_004432) do
+ActiveRecord::Schema.define(version: 2019_04_03_021424) do
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.string "place"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
